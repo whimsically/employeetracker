7f443a6db70a1ee519e26372b1e98fe6fa278ce1
Amanda Norris <amandakathleennorris@gmail.com>
2023-12-12 18:28:23 -0600

    added addEmployeePrompt

--- index.js
+++ index.js
@@ -13,32 +13,128 @@ const db = mysql.createConnection(
     }
   );
 
-  console.log('Welcome to the Employee Management System!');
-
-  inquirer
-  .prompt([
-    {
-        type: 'list',
-        name: 'first_menu',
-        message: 'What would you like to do?',
-        choices: ['View All Employees',
+//initial menu prompt
+const firstMenu = {
+    type: 'list',
+    name: 'firstMenu',
+    message: 'What would you like to do?',
+    choices: [
+        'View All Employees',
         'Add Employee',
         'Update Employee Role',
         'View All Roles',
-        'Add Roles',
+        'Add Role',
         'View All Departments',
         'Add Department',
         'Quit'
-        ]
-    }
-  ])
-  .then((answers) => {
-    // Use user feedback for... whatever!!
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
-  });
+    ]
+}
+
+//shows the firstMenu prompt
+function main() {
+    inquirer.prompt(firstMenu).then((answers) => {
+        switch (answers.firstMenu) {
+            //switch statement that runs a function based on what was selected
+            case 'View All Employees':
+                console.log('hii');
+                viewEmployees();
+                break;
+            case 'Add Employee':
+                addEmployeePrompt();
+                break;
+            case 'Update Employee Role':
+                updateEmployeeRole();
+                break;
+            case 'View All Roles':
+                viewRoles();
+                break;
+            case 'Add Role':
+                addRole();
+                break;
+            case 'View All Departments':
+                viewDepartments();
+                break;
+            case 'Add Department':
+                addDepartment();
+                break;
+            case 'Quit':
+                quitProgram();
+                break;
+        }
+    });
+}
+
+//views employees table
+function viewEmployees() {
+    db.query(`SELECT * FROM employees`, function (err, result) {
+        console.log(result);
+        main();
+    });
+}
+
+//views roles table
+function viewRoles() {
+    db.query(`SELECT * FROM role`, function (err, result) {
+        console.log(result);
+        main();
+    });
+}
+
+//views department table
+function viewDepartments() {
+    db.query(`SELECT * FROM department`, function (err, result) {
+        console.log(result);
+        main();
+    });
+}
+
+function addEmployeePrompt() {
+    inquirer.prompt([
+        {
+        type: 'input',
+        name: 'employeeFirstName',
+        message: 'Please enter the first name of the new employee'},
+        {
+        type: 'input',
+        name: 'employeeLastName',
+        message: 'Please enter the last name of the new employee'},
+        {
+        type: 'input',
+        name: 'employeeRole',
+        message: 'Please enter the role of the new employee'},
+        {
+        type: 'input',
+        name: 'employeeManager',
+        message: 'Please enter the manager of the new employee'},
+        ])
+        .then((response) => {
+            console.log(response.employeeFirstName);
+            addEmployeeSQL();
+            main();
+    })
+    
+}
+        
+
+function addEmployeeSQL() {
+
+}
+
+function updateEmployeeRole() {
+
+}
+
+function addRole() {
+
+}
+
+function addDepartment() {
+
+}
+
+function quitProgram() {
+
+}
+
+console.log('Welcome to the Employee Management System!');
+main();
